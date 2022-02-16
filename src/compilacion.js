7b3022801f4ab11f960064c2bb8f0701438db4f1
Aymara <aymara.g.e@gmail.com>
2022-02-16 11:38:03 -0300

    primeros pasos

---
+++ src/compilacion.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAN5A-PYnYar1iY3JtFkWnSEYGNNrH04-Y",
+  authDomain: "bazinga-aymlary22.firebaseapp.com",
+  projectId: "bazinga-aymlary22",
+  storageBucket: "bazinga-aymlary22.appspot.com",
+  messagingSenderId: "396585377938",
+  appId: "1:396585377938:web:d70f44b54d835342c7864b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,38 @@
 // aqui exportaras las funciones que necesites
+let container = document.getElementById("films.anime");
+ const logueo = () =>{
+  const[estaRegistrandose, setEstaRegistrandose] = useState(false);
+  return `
+  <container>
+      <Stack gap={3}>
+        <h1>{estaRegistrandose ? "Regístrate" : "Inicia Sesión"}</h1>
+      <Form>
+        <Form.group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter mail"></Form.Control>
+          <Form.Text className="text-muted">
+            We'll never Share your email with anyone else.
+          </Form.Text>
+        </Form.group>
 
-export const myFunction = () => {
-  // aqui tu codigo
-  console.log('Hola mundo!');
+        <Form.group className="mb-3"  controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Enter Password"></Form.Control>
+        </Form.group>
+
+        <Form.group className="mb-3"  controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" /> 
+        </Form.group>
+
+
+  <Button variant= "primary" type="submit"> </Button>
+
+
+  <Button variant="primary" onClick= {() => setEstaRegistrandose(!estaRegistrandose)}>
+    {estaRegistrandose ? "¿Ya tienes cuenta? Inicia Sesión" : "¿No tienes cuenta? Regístrate"}
+  </Button>
+      </Form>
+  </Stack>
+    </container>
+    `
 };
