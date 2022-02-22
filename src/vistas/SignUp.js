01f3bbb6fdcca8bfea246f38cf9d9f8c9bcf96f1
Aymara <aymara.g.e@gmail.com>
2022-02-22 19:30:32 -0300

    avances

--- src/vistas/SignUp.js
+++ src/vistas/SignUp.js
@@ -1,20 +1,19 @@
 
 export const signUp = () => {
     const divSignUp = document.createElement("div");
-    const viewSignUp =`
+    const viewSignUp = `
   <form id ="form-SignUp" class="form-SignUp">
      <h1>Regístrate</h1>
-    <form class="mb-3" id="signUpEmail">
+    
      <input type="text" placeholder="Usuario" id="user" required>
      <br>
      <input type="email" placeholder="correo electrónico" id="Email" required>
-    </form><br>
-    <form class="mb-3"  id="signUpPassword">
+     <br>
      <input type="password" placeholder="Contraseña" id="password" required>
      <br>
      <input type="password" placeholder="Contraseña" id="password1" required>
-    </form><br>
-  <button type="submit" class= "primary" id="button-SignUp">Regístrate</button>
+   <br>
+  <button type="button" class="primary" id="button-SignUp">Regístrate</button>
   </form>
   `
   divSignUp.innerHTML =viewSignUp;
@@ -23,5 +22,12 @@ export const signUp = () => {
   
   };
 
-  const password = document.getElementById
 
+ 
+  //
+  //console.log(btonsignup);
+ 
+  
+    
+ 
+  
---
+++ src/vistas/logueoregistrar.js
@@ -0,0 +1,13 @@
+//import {registrar } from "./main.js"; 
+ 
+
+//const btonsignup = document.querySelectorAll('button-SignUp');
+//console.log(btonsignup);
+
+
+ 
+    
+
+
+
+   
