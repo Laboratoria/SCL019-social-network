e9a29dd3f8aad878d5322327d1a3db79ad3db1b9
Larysa Yanushchyk <buha.autilla@gmail.com>
2022-03-03 16:59:18 -0300

    css grid y styles

--- src/vistas/welcome.js
+++ src/vistas/welcome.js
@@ -3,12 +3,15 @@ export const welcome= () => {
     welcomeScreen.className='welcome signInMain';
     welcomeScreen.innerHTML= `
     <div class= "contenedor-welcome gridContainer">
-    <img src= "" id="logo" class= "logo" >
+    
+    <img src= "images/logo-1.png" alt="Bazinga_Speech_Bubble" id="logo" class= "logo col-12">
     <form id ="form-welcome" class="form-welcome col-12">
+
     <button type="submit" class="primary" id="buttoninicia">¿Ya tienes cuenta? Inicia Sesión</button>
- 
     <button type="submit" class="primary"  id="buttonRegistrar">¿No tienes cuenta? Regístrate</button>
-    </form></div>`
+
+    </form>
+    </div>`
 ;
 
 welcomeScreen.querySelector('#buttoninicia').addEventListener('click', () => {
