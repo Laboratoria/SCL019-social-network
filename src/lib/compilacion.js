db998707ea76de6db3aa3605510748b2d8a600ad
Larysa Yanushchyk <buha.autilla@gmail.com>
2022-03-09 11:44:51 -0300

    comentado sendEmailVer linea 67

--- src/lib/compilacion.js
+++ src/lib/compilacion.js
@@ -64,14 +64,14 @@ export const register = (email, password) => {
     user.displayName = userName;
     console.log(userName);
     
-    sendEmailVerification(auth.currentUser)
-    .then(() => {
-        alert("Email verification sent!")
-    }).catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode, errorMessage);
-    });
+    // sendEmailVerification(auth.currentUser)
+    // .then(() => {
+    //     alert("Email verification sent!")
+    // }).catch((error) => {
+    //     const errorCode = error.code;
+    //     const errorMessage = error.message;
+    //     console.log(errorCode, errorMessage);
+    // });
 
     }).catch((error) => {
         const errorCode = error.code;
@@ -88,14 +88,6 @@ export const register = (email, password) => {
         if (errorCode === "auth/email-already-in-use") {
             alert ("Ya estás Registrad@")}
     });
-    // sendEmailVerification(auth.currentUser)
-    //     .then(() => {
-    //         alert("Email verification sent!")
-    //     }).catch((error) => {
-    //         const errorCode = error.code;
-    //         const errorMessage = error.message;
-    //         console.log(errorCode, errorMessage);
-    // });
 }
 
 // Iniciar Sesión con Usuario Guardado
@@ -108,19 +100,19 @@ export const iniciaSesion = (email, password) => {
         console.log(user);
         // ...
         window.location.hash='#/muro';
-    // }).catch((error) => {
-    //     const errorCode = error.code;
-    //     const errorMessage = error.message;
-    //     console.log(errorCode); 
-    //     console.log(errorMessage);
-    //     if (errorCode === "auth/missing-email") {
-    //         alert ("Ingresa un correo")}
-    //     if (errorCode === "auth/invalid-email") {
-    //         alert ("Ingresa un correo válido")}
-    //     if (errorCode === "auth/internal-error") {
-    //         alert ("Error! Intenta con datos correctos")}
-    //     if (errorCode === "auth/wrong-password") {
-    //         alert ("Tú contraseña es inválida")}
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode); 
+        console.log(errorMessage);
+        if (errorCode === "auth/missing-email") {
+            alert ("Ingresa un correo")}
+        if (errorCode === "auth/invalid-email") {
+            alert ("Ingresa un correo válido")}
+        if (errorCode === "auth/internal-error") {
+            alert ("Error! Intenta con datos correctos")}
+        if (errorCode === "auth/wrong-password") {
+            alert ("Tú contraseña es inválida")}
     });
 }
 
