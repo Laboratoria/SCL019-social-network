436a784ead877f35ca322d8d8f8ae3046e67d355
teregonzalez <teregonzalezpro@gmail.com>
2022-03-10 12:10:08 -0300

    Se agregan test unitarios en validate.spec.js

--- test/index.spec.js
+++
@@ -1,8 +0,0 @@
-// importamos la funcion que vamos a testear
-import { myFunction } from '../src/App/index';
-
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
-  });
-});
