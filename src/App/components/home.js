e57bee535c41d2bd7e4426ff57f4e2e2e62575d7
GabrielaDana <gabriela.dana.pacheco@gmail.com>
2022-03-18 20:03:55 -0300

    intento fallido

--- src/App/components/home.js
+++ src/App/components/home.js
@@ -52,7 +52,6 @@ export const home = (autenticacion, db) => {
 
   getPost(db);
 
-
   // const anuncio = document.createElement('div');
   // anuncio.className = 'anuncio';
   // contPost.appendChild(anuncio);
